1a9f357a503bb36f4e5846fdcb60600e52302d0a
Ruslan <ruslan.vafin.93@gmail.com>
2022-08-01 23:55:29 +0300

    add homepage locator and methods

---
+++ pom/homepage_nav.py
@@ -0,0 +1,17 @@
+from base.base import SeleniumBase
+from selenium.webdriver.remote.webelement import WebElement
+from typing import List
+
+
+class HomepageNav(SeleniumBase):
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver = driver
+        self.__nav_links: str = '#mainNavigationFobs>li'
+
+    def get_nav_links(self) -> List[WebElement]:
+        return self.are_visible('css', self.__nav_links, 'Header Navigation Links')
+
+    def get_nav_links_text(self) -> str:
+        nav_links = self.get_nav_links()
+        nav_links_text = [link for link in nav_links]
